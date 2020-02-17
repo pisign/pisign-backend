e47e193ca05e7e6148abee95fb8780985284b103
Noah Yoshida <nyoshida@nd.edu>
2020-02-17 16:40:25 -0500

    added base message so we can pass status to frontend (original commit) + [AUTO SPEC] Spec update

---
+++ types/message.go
@@ -0,0 +1,15 @@
+package types
+
+type MessageStatus string
+
+const (
+	StatusSuccess MessageStatus = "success"
+	StatusFailure MessageStatus = "failure"
+)
+
+// BaseMessage is the base message we are sending to frontend
+// All structs being send to the frontend should inherit from this
+type BaseMessage struct {
+	Status       MessageStatus
+	ErrorMessage string
+}
