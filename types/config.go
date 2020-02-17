270948937b05770ed0b6205a4c3d042dc0738210
Edward Atkinson <eatkins1@nd.edu>
2020-02-16 22:47:03 -0500

    made  Position struct's internal fields all private

--- types/config.go
+++ types/config.go
@@ -1,10 +1,10 @@
 package types
 
-// Position struct
+// Position information for api widgets
 type Position struct {
-	X int `json:"x"`
-	Y int  `json:"y"`
-	W int `json:"w"`
-	H int `json:"h"`
-	I string `json:"i"`
+	x int
+	y int
+	w int
+	h int
+	i string
 }
